6d23bc096f640ba35657c5c8782da309288c6d1d
vg <vengadoravg@gmail.com>
2016-09-05 14:18:15 -0400

    Textura de cholita e inventario y bloqueo

---
+++ SuriAlFuturo/Assets/Scripts/CollectionSystem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CollectionSystem : MonoBehaviour {
+    public List<Collectable> IngameCollectables;
+    public Collector IngameCollector;
+
+    public UIInventory UIController;
+
+    public void RegisterCollectable (Collectable collectable) {
+        if (IngameCollectables == null) {
+            IngameCollectables = new List<Collectable>();
+        }
+        IngameCollectables.Add(collectable);
+        IngameCollector.RegisterCollectable(collectable);
+    }
+
+    void Start () {
+        if (!IngameCollector) {
+            throw (new UnityException("who is the collector? (PROTIP: Collector prefab to Player as a child, and drag it into \"Ingame Collector\" field)"));
+        }
+
+        if (!UIController) {
+            throw(new UnityException("I need a canvas with a UI Inventory component attached, when created, drag it into the UI Controller field!"));
+        }
+    }
+
+    void Update () {
+        
+    }
+
+}
---
+++ SuriAlFuturo/Assets/Scripts/Collector.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Collector : MonoBehaviour {
+    public Dictionary<Sprite, bool> Inventory = new Dictionary<Sprite, bool>();
+
+    private CollectionSystem _controller;
+    
+    void Start () {
+        _controller = GameObject.FindGameObjectWithTag(Tag.GameController)
+            .GetComponent<CollectionSystem>();
+    }
+    
+    void Update () {
+    }
+
+    public void RegisterCollectable (Collectable collectable) {
+        Inventory.Add(collectable.Image, false);
+    }
+
+    public void Take (Collectable collectable) {
+        Inventory[collectable.Image] = true;
+        _controller.UIController.AddItem(collectable);
+    }
+
+    public void Give (Collectable collectable) {
+        Inventory[collectable.Image] = false;
+        _controller.UIController.RemoveItem(collectable);
+    }
+}
