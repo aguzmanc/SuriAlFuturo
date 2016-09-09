875fdbefd401b4e6346ed514568c0620e2189ae6
arn <spacerockganimedes@gmail.com>
2016-09-09 03:21:49 -0400

    Sistema de control de barco y carga/descarga de Suri de tierra - agua

---
+++ SuriAlFuturo/Assets/Scripts/ShipUnloadReceiver.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipUnloadReceiver : MonoBehaviour 
+{
+    public Ship Ship;
+}
---
+++ SuriAlFuturo/Assets/Scripts/Suri.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Suri : MonoBehaviour 
+{
+    private static GameObject _instance;
+    public static GameObject Instance
+    {
+        get {
+            return _instance;
+        }
+    }
+
+
+	void Start () 
+    {
+        _instance = gameObject;
+	}
+}
