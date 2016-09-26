ff55f5cd836bf39b27473a9df5145bf076e138ed
vg <vengadoravg@gmail.com>
2016-09-26 17:53:10 -0400

    Primer nivel casi listo (hay que mover el portal cerca de la rampa x_x

--- SuriAlFuturo/Assets/Scripts/FollowCamera.cs
+++ SuriAlFuturo/Assets/Scripts/FollowCamera.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 using System.Collections;
 
 public class FollowCamera : MonoBehaviour {
-    public bool IsInHole;
+    public const int IN_FLOOR = 0;
+    public const int IN_HOLE = 1;
+    public const int IN_WATER = 2;
+
+    public int State = 0;
     public GameObject Target;
-    public float NoHoleDistance = 15;
+    public float FloorDistance = 15;
     public float HoleDistance = 25;
+    public float WaterDistance = 30;
     public GameObject TheCamera;
 
-    public Vector3 NoHoleRotation = new Vector3(60, 45, 0);
+    public Vector3 FloorRotation = new Vector3(60, 45, 0);
     public Vector3 HoleRotation = new Vector3(90, 45, 0);
+    public Vector3 WaterRotation = new Vector3(45, 45, 0);
     public float TransitionTime = 5;
 
     private float _timeOnState = 0;
@@ -23,10 +29,10 @@ public class FollowCamera : MonoBehaviour {
     public Vector3 Forward
     {
         get {
-            float y = IsInHole ? HoleRotation.y : NoHoleRotation.y;
+            float y = GetY();
 
             return new Vector3(Mathf.Cos(((90-y)/360) * 2 * Mathf.PI), 0,
-                Mathf.Cos((y/360) * 2 * Mathf.PI)); 
+                Mathf.Cos((y/360) * 2 * Mathf.PI));
         }
     }
 
@@ -51,57 +57,88 @@ public class FollowCamera : MonoBehaviour {
 
 
 
-    
-    void Start () 
+
+    void Start ()
     {
         _instance = this;
-        
-        TheCamera.transform.localPosition = new Vector3(0,0, -NoHoleDistance);
-        transform.rotation = Quaternion.Euler(NoHoleRotation);
-    }
-    
-    void Update () 
-    {
-        Vector3 a, b;
-        float c, d;
 
+        TheCamera.transform.localPosition = new Vector3(0,0, -FloorDistance);
+        transform.rotation = Quaternion.Euler(FloorRotation);
+    }
+
+    void Update ()
+    {
         _timeOnState += Time.deltaTime;
         this.transform.position = Target.transform.position;
 
-        if (IsInHole) {
-            a = _cachedRotation; // TODO
-            b = HoleRotation;
-            c = _cachedDistance;
-            d = HoleDistance;
-        } else {
-            b = NoHoleRotation;
-            a = _cachedRotation;
-            d = NoHoleDistance;
-            c = _cachedDistance;
-        }
+        Vector3 rotationOrigin, rotationDestination;
+        float distanceOrigin, distanceDestination;
 
+        rotationOrigin = _cachedRotation;
+        distanceOrigin = _cachedDistance;
+
+
+        switch (State) {
+            case IN_FLOOR:
+                rotationDestination = FloorRotation;
+                distanceDestination = FloorDistance;
+                break;
+            case IN_HOLE:
+                rotationDestination = HoleRotation;
+                distanceDestination = HoleDistance;
+                break;
+            default: // IN_WATER
+                rotationDestination = WaterRotation;
+                distanceDestination = WaterDistance;
+                break;
+        }
+        
         transform.rotation =
-            Quaternion.Euler(Vector3.Slerp(a, b, _timeOnState/TransitionTime));
+            Quaternion.Euler(Vector3.Slerp(rotationOrigin, rotationDestination,
+                                           _timeOnState / TransitionTime));
 
         TheCamera.transform.localPosition =
-            new Vector3(0,0, -Mathf.Lerp(c, d, _timeOnState/TransitionTime));
+            new Vector3(0,0, -Mathf.Lerp(distanceOrigin, distanceDestination,
+                                         _timeOnState / TransitionTime));
     }
 
-    public void OnHoleEnter () 
+    public void OnHoleEnter ()
     {
+        _SwitchState(IN_HOLE);
+    }
+
+    public void OnHoleExit ()
+    {
+        _SwitchState(IN_FLOOR);
+    }
+
+    public void EnterWater () {
+        _SwitchState(IN_WATER);
+    }
+
+    public void ExitWater () {
+        _SwitchState(IN_FLOOR);
+    }
+
+    private void _SwitchState (int newState) {
         _timeOnState = 0;
-        IsInHole = true;
+        State = newState;
         _cachedRotation = TheCamera.transform.rotation.eulerAngles;
         _cachedDistance = Mathf.Abs(TheCamera.transform.localPosition.z);
     }
 
-    public void OnHoleExit () 
-    {
-        _timeOnState = 0;
-        IsInHole = false;
-        _cachedRotation = TheCamera.transform.rotation.eulerAngles;
-        _cachedDistance = Mathf.Abs(TheCamera.transform.localPosition.z);
+    public float GetY () {
+        switch (State) {
+            case IN_WATER:
+                return WaterRotation.y;
+                break;
+            case IN_FLOOR:
+                return FloorRotation.y;
+                break;
+            case IN_HOLE:
+                return HoleRotation.y;
+                break;
+        }
+        return 0;
     }
-
-
 }
---
+++ SuriAlFuturo/Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    private bool _drivingBoat;
+    public FollowCamera FollowingCamera;
+
+    public void SetDrivingBoat (bool value) {
+        _drivingBoat = value;
+        if (value) {
+            FollowingCamera.EnterWater();
+        } else {
+            FollowingCamera.ExitWater();
+        }
+    }
+}
