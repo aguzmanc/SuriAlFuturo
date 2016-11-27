3c8b28fbf4d2729a9691ab6ffe05e86bfb0ba5da
vg <vengadoravg@gmail.com>
2016-11-27 13:10:23 -0400

    nuevo font, integrados diálogos y añadida biologa

---
+++ SuriAlFuturo/Assets/Scripts/BiologaAnimations.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BiologaAnimations : MonoBehaviour {
+    public float MinIdleTime = 1;
+    public float MaxIdleTime = 4;
+
+    private Animator _animator;
+    
+    void Start () {
+        _animator = GetComponent<Animator>();
+        StartCoroutine("RandomTrigger");
+    }
+    
+    IEnumerator RandomTrigger () {
+        _animator.SetTrigger("RandomAction");            
+    
+        yield return new WaitForSeconds(Random.Range(MinIdleTime, MaxIdleTime));
+        StartCoroutine("RandomTrigger");
+    }
+}
