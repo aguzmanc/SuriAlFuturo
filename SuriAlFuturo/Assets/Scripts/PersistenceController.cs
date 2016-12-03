5aafc49e950603d20801b06d6c7d4a3df70104cc
arn <spacerockganimedes@gmail.com>
2016-12-03 14:21:19 -0400

    Primera Version de ChapterController para guardar avances del juego

--- SuriAlFuturo/Assets/Scripts/PersistenceController.cs
+++ SuriAlFuturo/Assets/Scripts/PersistenceController.cs
@@ -1,21 +1,85 @@
 using UnityEngine;
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
-public class PersistenceController : MonoBehaviour {
+[Serializable]
+public class PersistentChapter{
+    public int ChapterNumber;
+}
+
+public class PersistenceController : MonoBehaviour 
+{
     public Dictionary<Vector3, PersistedGameObject> SavedGameObjects =
         new Dictionary<Vector3, PersistedGameObject>();
 
-    public void Save (Persist gameObject) {
+
+    public static string FileName = "ReachedChapter.dat";
+
+    public static int ReachedChapter;
+
+
+    void Awake()
+    {   
+        _LoadDataFromDisk();
+    }
+
+
+    public void Save (Persist gameObject) 
+    {
         SavedGameObjects[gameObject.PersistenceKey] =
             gameObject.GetPersistenceObject();
     }
 
-    public void Load (Persist gameObject) {
+
+
+    public void Load (Persist gameObject) 
+    {
         gameObject.Load(SavedGameObjects[gameObject.PersistenceKey]);
     }
 
-    public bool HasSavedData (Persist gameObject) {
+
+
+    public bool HasSavedData (Persist gameObject) 
+    {
         return SavedGameObjects.ContainsKey(gameObject.PersistenceKey);
+    }
+
+
+
+    public static void SaveDataToDisk() 
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/" + FileName);
+
+        PersistentChapter chapter = new PersistentChapter();
+        chapter.ChapterNumber = ReachedChapter;
+
+        bf.Serialize(file, chapter);
+        file.Close();
+    }
+
+
+
+    private static void _LoadDataFromDisk()
+    {
+        string nombreArch = Application.persistentDataPath + "/" + FileName;
+
+        if(File.Exists(nombreArch)){
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(nombreArch, FileMode.Open);
+
+            PersistentChapter chapter = (PersistentChapter)bf.Deserialize(file);
+            file.Close();
+
+            ReachedChapter = chapter.ChapterNumber;
+        } else  {
+            ReachedChapter = 0;
+            SaveDataToDisk();
+        }
+            
     }
 }
