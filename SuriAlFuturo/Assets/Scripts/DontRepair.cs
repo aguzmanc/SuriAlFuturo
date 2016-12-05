2c09cea36c5054ca81c032cccb4e1a0b95a9ca80
vg <vengadoravg@gmail.com>
2016-12-05 19:45:36 -0400

    NO IREMOS A LA CARCEL :D

---
+++ SuriAlFuturo/Assets/Scripts/DontRepair.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SuriAlFuturo;
+
+public class DontRepair : MonoBehaviour {
+    // public List<Collectable.Tag> OldItems;
+    // public TextAsset Message;
+
+    // private Dialogue[] _digestedMessage;
+    // private CollectionSystem _collectionController;
+    // private RepairController _controller;
+
+    // void Start () {
+    //     _collectionController = GameObject.FindGameObjectWithTag(Tag.GameController)
+    //         .GetComponent<CollectionSystem>();
+    //     _controller = _collectionController.GetComponent<RepairController>();
+    //     _controller.CantRepair.Add(this);
+    // }
+
+    // void OnDestroy () {
+    //     _controller.Repairs.Remove(this);
+    // }
+
+    // public bool CanRepair () {
+    //     foreach (Collectable.Tag oldItem in OldItems) {
+    //         // if (!_collectionController.HasItem(oldItem)
+    //     }
+    // }
+    
+    // void Update () {
+	
+    // }
+}
