0fd7d37303097b259fa20d438b8f45658da124f7
arn <spacerockganimedes@gmail.com>
2017-01-05 23:40:46 -0400

    Fix Bug #05: Es posible cerrar primero el grifo luego hablar a Joaquín, de todas formas le dice “Hay una pila abierta allí”..  Sol: Habilitar la opción de cerrarla luego de haber hablado con Joaquín por primera vez.

--- SuriAlFuturo/Assets/Scripts/OnReadSwitch.cs
+++ SuriAlFuturo/Assets/Scripts/OnReadSwitch.cs
@@ -1,23 +1,32 @@
 using UnityEngine;
 using System.Collections;
+using SuriAlFuturo;
 
 public class OnReadSwitch : MonoBehaviour {
     public Talkable TheTalkable;
 
     public int OnDialogueIndexRead;
     public int SwitchTo;
+    public Event TriggeredEvent;
+
+    private EventController _controller;
     
     void Start () {
         if (TheTalkable == null) {
             TheTalkable = GetComponent<Talkable>();
         }
+
+        _controller = GameObject.FindGameObjectWithTag(Tag.GameController)
+            .GetComponent<EventController>();
     }
 
     void Update () {
-	if (TheTalkable.GetDialogueIndex() == OnDialogueIndexRead &&
+	    if (TheTalkable.GetDialogueIndex() == OnDialogueIndexRead &&
             TheTalkable.WasRead) {
             TheTalkable.SetDialogueIndex(SwitchTo);
             this.enabled = false;
+
+            _controller.TriggerEvent(TriggeredEvent);
         }
     }
 }
