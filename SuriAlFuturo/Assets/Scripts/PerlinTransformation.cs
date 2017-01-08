1b32fd0e313095091306b190109a461e90c6ff60
arn <spacerockganimedes@gmail.com>
2017-01-07 22:57:58 -0400

    Fix Bug #42:  El drone que sigue a Suri, es muy grande, sus hélices no votan, y la sigue de forma lineal siempre sobre ella, no parece volar.

---
+++ SuriAlFuturo/Assets/Scripts/PerlinTransformation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerlinTransformation : MonoBehaviour 
+{
+    Vector2 _posPerlin;
+    Vector2 _vPerlin;
+
+    public float Velocity = 0.1f;
+    public float Amplitude = 3f;
+    public float Offset = 0f;
+
+    public bool PosX = false;
+    public bool PosY = false;
+    public bool PosZ = false;
+
+    public bool RotX = false;
+    public bool RotY = false;
+    public bool RotZ = false;
+
+    public float Value;
+
+    void Start () 
+    {
+        _posPerlin = new Vector2();
+        _vPerlin = Random.insideUnitCircle;
+    }
+
+
+
+    void Update () 
+    {
+        Value = ((Mathf.PerlinNoise(_posPerlin.x, _posPerlin.y) * Amplitude * 2) - Amplitude) + Offset;
+        _posPerlin += (_vPerlin * Velocity);
+
+        //transform.localPosition = new Vector3(PosX ? noise : 0, PosY ? noise : 0, PosZ ? noise : 0);
+        //transform.localRotation = Quaternion.Euler( new Vector3(RotX?noise:0,RotY?noise:0,RotZ?noise:0));
+    }
+}
---
+++ SuriAlFuturo/Assets/Scripts/SmoothFollower.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollower : MonoBehaviour 
+{
+    public Transform Target;
+    public float Smoothness = 0.1f;
+
+	
+	void Start () {
+	
+	}
+	
+	
+	void Update () 
+    {
+        Vector3 to = new Vector3(Target.position.x, transform.position.y, Target.position.z);
+        transform.position = Vector3.Lerp(transform.position, to, Smoothness * Time.deltaTime);
+	}
+}
