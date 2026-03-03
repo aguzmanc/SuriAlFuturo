955b37928ff4394db9de4acc4dd91d38b0db1f1b
aguzmanc <spacerockganimedes@gmail.com>
2026-03-03 12:09:52 -0400

    Rana y cambios pequenhos

--- SuriAlFuturo/Assets/Scripts/UIController.cs
+++ SuriAlFuturo/Assets/Scripts/UIController.cs
@@ -48,7 +48,7 @@ public class UIController : MonoBehaviour
 
         TextFin.GetComponent<FadeInFadeOut>().FadeIn();
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
 
         ButtonGoToCredits.SetActive(true);
     }
